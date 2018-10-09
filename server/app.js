9be903ff86cdcc060b39abb938d5d1a423e04031
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-09 22:28:36 +0530

    graphql schema added to graphql server

--- server/app.js
+++ server/app.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
+const schema = require('./models/schema')
 
 const app = express()
-
-app.use('/graphql', graphqlHTTP({}))
+app.use('/', (req, res) => {
+  res.send('graphql server')
+})
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema
+  })
+)
 
 app.listen(4000, () => {
   console.log('now listening for requests on port 4000')
