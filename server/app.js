7019b00a309051b76875ee8287ae009327a571f6
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-10 15:29:46 +0530

    mongoose added

--- server/app.js
+++ server/app.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema = require('./models/schema')
+const mongoose = require('mongoose')
 
+const keys = require('./config/keys')
 const app = express()
+
+// connect to mongo db
+mongoose.connect(keys.mongoURI)
 
 app.use(
   '/graphql',
