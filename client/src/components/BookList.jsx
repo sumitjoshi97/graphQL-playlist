2c80989c9c5fc7c03051a2998adde2eefa9f3ea1
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-10 20:34:56 +0530

    book list component added

---
+++ client/src/components/BookList.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class BookList extends Component {
+  render() {
+    return (
+      <div>
+        <ul id="book-list">
+          <li>book name</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default BookList
