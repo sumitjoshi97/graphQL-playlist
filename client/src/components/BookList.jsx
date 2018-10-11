baedf8313b9444ddc1e326827a684f1e5633fe7d
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-11 11:16:34 +0530

    queris data from graphql server

--- client/src/components/BookList.jsx
+++ client/src/components/BookList.jsx
@@ -1,15 +1,25 @@
 import React, { Component } from 'react'
+import { graphql } from 'react-apollo'
+import { getBooksQuery } from '../queries/queries'
 
 export class BookList extends Component {
+  displayBooks = () => {
+    let data = this.props.data
+    if (data.loading) {
+      return <div>Loading books...</div>
+    } else {
+      return data.books.map(book => <li key={book.id}>{book.name}</li>)
+    }
+  }
+
   render() {
+    console.log(this.props)
     return (
       <div>
-        <ul id="book-list">
-          <li>book name</li>
-        </ul>
+        <ul id="book-list">{this.displayBooks()}</ul>
       </div>
     )
   }
 }
 
-export default BookList
+export default graphql(getBooksQuery)(BookList)
