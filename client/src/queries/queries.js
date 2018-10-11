3159c000ec6d9c3b3439d06a729ecb185e37abda
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-11 12:09:58 +0530

    addBookMutation query updated with variables

--- client/src/queries/queries.js
+++ client/src/queries/queries.js
@@ -19,7 +19,10 @@ export const getAuthorsQuery = gql`
 `
 
 export const addBookMutation = gql`
-  mutation {
-    addBook(book: "", genre: "", authorId: "")
+  mutation($name: String!, $genre: String!, $authorId: ID!) {
+    addBook(name: $name, genre: $genre, authorId: $authorId) {
+      name,
+      id
+    }
   }
 `
