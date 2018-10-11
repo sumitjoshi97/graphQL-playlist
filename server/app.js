1ab648bc2f16a49d7beadb6a7dfb5d739a30b4da
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-11 11:16:45 +0530

    cors allowed

--- server/app.js
+++ server/app.js
@@ -2,9 +2,13 @@ const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema = require('./models/schema')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const keys = require('./config/keys')
 const app = express()
+
+// allow cross-orign-request
+app.use(cors())
 
 // connect to mongo db
 mongoose.connect(keys.mongoURI)
